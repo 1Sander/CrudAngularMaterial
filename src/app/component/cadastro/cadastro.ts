4e3c6e27b7168bd678f01ac8b8be3ea5de7ff7eb
Wallace Sander <90098336+1Sander@users.noreply.github.com>
2025-11-05 21:07:31 -0300

    gitchanges

---
+++ src/app/component/cadastro/cadastro.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { Cliente } from './cliente';
+import { ClienteService } from '../../services/cliente/cliente.service';
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [FlexLayoutModule,
+            MatCardModule, 
+            FormsModule, 
+            MatFormFieldModule,
+            MatInputModule,
+            MatIconModule,
+            MatButtonModule,
+          ],
+  templateUrl: './cadastro.html',
+  styleUrl: './cadastro.scss',
+})
+export class Cadastro {
+
+  cliente: Cliente = Cliente.newCliente();
+
+  constructor(private service: ClienteService){
+
+  }
+
+  salvar(){
+    this.service.salvar(this.cliente);
+  }
+}
